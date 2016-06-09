dd98922372e22700fd21b539c7087275e95f3f01
Jacob Williams <jake@jacobwilliams.me>
2016-06-09 07:45:00 -0700

    Increase zoom to level before angled view

--- src/app/map.ts
+++ src/app/map.ts
@@ -6,7 +6,7 @@ export class MapComponent extends google.maps.Map {
   constructor() {
     super(document.getElementById('map'), {
       center: new google.maps.LatLng(36.988363, -121.9566037),
-      zoom: 16,
+      zoom: 17,
       mapTypeId: google.maps.MapTypeId.HYBRID,
       mapTypeControlOptions: {
         position: google.maps.ControlPosition.LEFT_BOTTOM
